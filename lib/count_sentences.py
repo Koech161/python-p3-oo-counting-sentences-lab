c6c955ef054b5d92e22315a3c4896e7cf1e8bd03
Abel Koech <koechabel161@gmail.com>
2024-08-30 20:15:04 +0300

    endswitch

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
-
+import re
 class MyString:
-  pass
+   
+    def __init__(self, value=''):
+      
+          self.value = value
+    @property
+    def value(self):
+         return self._value
+    @value.setter
+    def value(self, new_value):
+         if isinstance(new_value,str):
+              self._value =new_value 
+         else:
+              print('The value must be a string.')
+              self._value=''
+                       
+    def is_sentence(self):
+        return self._value.endswith('.')
+    def is_question(self):
+         return self._value.endswith('?')
+    def is_exclamation(self):
+         return self._value.endswith('!')
+    def count_sentences(self):
+         sentences = re.split(r'[!?.]', self._value)
+         return len([s for s in sentences if s.strip()])
+      
+string = MyString("This is a string! It has three sentences. Okey.Right?")
+print(string.count_sentences()) 
+# my_string =MyString('Hello World.')
+# print(my_string.is_sentence())
+
+# my_string2 =MyString('Hello World!')
+# print(my_string2.is_sentence())
